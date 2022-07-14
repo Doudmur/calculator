e51a320f8b8d5c112efda8ff8d15922704487b5c
Doudmur <bbeka544@mail.ru>
2022-07-14 17:24:12 +0600

    Calculator with writer

--- src/calculator.java
+++ src/calculator.java
@@ -1,10 +1,13 @@
 import java.lang.Exception;
 import java.util.Scanner;
 import java.io.File;
+import java.io.PrintWriter;
 
 class calculator {
     public static void main(String[] args) throws Exception{
-        File fl = new File("C:\\Users\\kaiyr\\IdeaProjects\\calculator\\src\\input.txt");
+        File fl = new File("input.txt");
+        File fileWriter = new File("output.txt");
+        PrintWriter pw = new PrintWriter(fileWriter);
         Scanner sc = new Scanner(fl);
         double first = 0, second = 0;
         String a = sc.nextLine(), znak;
@@ -13,7 +16,7 @@ class calculator {
         try {
             first = Double.parseDouble(b[0]);
         } catch(NumberFormatException e){
-            System.out.println("Error! Not number");
+            pw.println("Error! Not number");
             proverka = false;
         }
 
@@ -21,7 +24,7 @@ class calculator {
             try {
                 second = Double.parseDouble(b[2]);
             } catch(NumberFormatException e){
-                System.out.println("Error! Not number");
+                pw.println("Error! Not number");
                 proverka = false;
             }
         }
@@ -31,20 +34,21 @@ class calculator {
                 if(b[1].equals("+") || b[1].equals("-") || b[1].equals("/") || b[1].equals("*"));
                 else throw new Exception("OperationError");
             } catch (Exception e){
-                System.out.println("Operation Error!");
+                pw.println("Operation Error!");
                 proverka = false;
             }
 
         }
         if(proverka){
-            if(b[1].equals("+")) System.out.println(first + second);
-            if(b[1].equals("-")) System.out.println(first - second);
-            if(b[1].equals("*")) System.out.println(first * second);
+            if(b[1].equals("+")) pw.println(first + second);
+            if(b[1].equals("-")) pw.println(first - second);
+            if(b[1].equals("*")) pw.println(first * second);
             if(b[1].equals("/")) {
-                if(second == 0) System.out.println("Error! Division by zero");
-                else System.out.println(first / second);
+                if(second == 0) pw.println("Error! Division by zero");
+                else pw.println(first / second);
             }
 
         }
+        pw.close();
     }
 }
