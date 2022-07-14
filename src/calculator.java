fe24a72ffa8babdaeb31b1fcff1647ccb86070e4
Doudmur <bbeka544@mail.ru>
2022-07-14 17:28:58 +0600

    Calculator can read few lines

--- src/calculator.java
+++ src/calculator.java
@@ -9,45 +9,47 @@ class calculator {
         File fileWriter = new File("output.txt");
         PrintWriter pw = new PrintWriter(fileWriter);
         Scanner sc = new Scanner(fl);
-        double first = 0, second = 0;
-        String a = sc.nextLine(), znak;
-        String b[] = a.split(" ");
-        boolean proverka = true;
-        try {
-            first = Double.parseDouble(b[0]);
-        } catch(NumberFormatException e){
-            pw.println("Error! Not number");
-            proverka = false;
-        }
-
-        if(proverka){
+        while(sc.hasNextLine()){
+            double first = 0, second = 0;
+            String a = sc.nextLine(), znak;
+            String b[] = a.split(" ");
+            boolean proverka = true;
             try {
-                second = Double.parseDouble(b[2]);
+                first = Double.parseDouble(b[0]);
             } catch(NumberFormatException e){
                 pw.println("Error! Not number");
                 proverka = false;
             }
-        }
 
-        if(proverka){
-            try {
-                if(b[1].equals("+") || b[1].equals("-") || b[1].equals("/") || b[1].equals("*"));
-                else throw new Exception("OperationError");
-            } catch (Exception e){
-                pw.println("Operation Error!");
-                proverka = false;
+            if(proverka){
+                try {
+                    second = Double.parseDouble(b[2]);
+                } catch(NumberFormatException e){
+                    pw.println("Error! Not number");
+                    proverka = false;
+                }
             }
 
-        }
-        if(proverka){
-            if(b[1].equals("+")) pw.println(first + second);
-            if(b[1].equals("-")) pw.println(first - second);
-            if(b[1].equals("*")) pw.println(first * second);
-            if(b[1].equals("/")) {
-                if(second == 0) pw.println("Error! Division by zero");
-                else pw.println(first / second);
-            }
+            if(proverka){
+                try {
+                    if(b[1].equals("+") || b[1].equals("-") || b[1].equals("/") || b[1].equals("*"));
+                    else throw new Exception("OperationError");
+                } catch (Exception e){
+                    pw.println("Operation Error!");
+                    proverka = false;
+                }
 
+            }
+            if(proverka){
+                if(b[1].equals("+")) pw.println(first + second);
+                if(b[1].equals("-")) pw.println(first - second);
+                if(b[1].equals("*")) pw.println(first * second);
+                if(b[1].equals("/")) {
+                    if(second == 0) pw.println("Error! Division by zero");
+                    else pw.println(first / second);
+                }
+
+            }
         }
         pw.close();
     }
